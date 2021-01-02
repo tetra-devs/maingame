296c2673b00cccf4859dc0502f250b3ba9b1a868
Sayantan Biswas <sbnimo75@gmail.com>
2021-01-02 16:47:57 +0530

    added something

---
+++ scenes/main.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+onready var abc = $YSort/Player/DialogueSystem
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	abc.visible = true
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
